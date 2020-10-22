9f6531e8448b47a764dbb9b7255a290d34221a72
pengjiahao0 <2581212221@qq.com>
2020-10-22 10:53:40 +0800

    分支测试

--- src/main/java/com/echopeng/git/HelloGit.java
+++ src/main/java/com/echopeng/git/HelloGit.java
@@ -7,5 +7,6 @@ package com.echopeng.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!!");
+        System.out.println("分支测试");
     }
 }
