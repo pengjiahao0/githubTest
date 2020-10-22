a237d80bf8bcfc93d75244c90265773e29a9c0da
pengjiahao0 <2581212221@qq.com>
2020-10-22 11:25:26 +0800

    克隆添加

--- src/main/java/com/echopeng/git/HelloGit.java
+++ src/main/java/com/echopeng/git/HelloGit.java
@@ -11,5 +11,6 @@ public class HelloGit {
         System.out.println("分支添加");
         System.out.println("master添加");
         System.out.println("在线更新");
+        System.out.println("克隆添加");
     }
 }
