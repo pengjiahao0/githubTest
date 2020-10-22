3edcb27ff052d2c070e6b10acc1f9837473a283e
pengjiahao0 <2581212221@qq.com>
2020-10-22 10:45:30 +0800

    添加测试

---
+++ src/main/java/com/echopeng/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.echopeng.git;
+
+/**
+ * @Author: Echo Pen
+ * @Create_Date: 2020-10-22-10:33
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!");
+    }
+}
